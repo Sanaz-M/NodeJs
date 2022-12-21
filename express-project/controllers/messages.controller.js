4b166054e04fbd100cbba6ab3882193c8ea970c3
Sanaz M <sanazmohammaddi@gmail.com>
2022-12-21 16:04:33 +0100

    import media to message controller

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,5 +1,10 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 export function getMessages(req, res) {
-  res.send("Hey there!")
+  // res.send("Hey there!");
+  const mediaPath = join(dirname(fileURLToPath(import.meta.url)) , "../public/pic1.jpg");
+   res.sendFile(mediaPath);
 };
 
 export function postMessage(req, res) {
