03223c3746fd5b6a8133c1b8b53a5d132d46ec6e
Sanaz M <sanazmohammaddi@gmail.com>
2023-04-20 13:45:28 +0200

    build public folder and install morgan

--- NASA/server/src/app.js
+++ NASA/server/src/app.js
@@ -1,12 +1,26 @@
 import express from "express";
 import { planetsRouter } from '../routes/planets/planets.router.js';
 import cors from "cors";
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import morgan from "morgan";
+
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000/"
+}));
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(planetsRouter);
+app.use(express.static(join(dirname(fileURLToPath(import.meta.url)), "../public")))
+
+app.get('/', (req, res) => {
+  res.sendFile(join(dirname(fileURLToPath(import.meta.url)), "../public/index.html"))
+})
 
 export default app;
 
